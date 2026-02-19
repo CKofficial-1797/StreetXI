173550b8ec60e011986f462d9dd92f443e8b304c
CKofficial-1797 <sanjaygorain1797@gmail.com>
2026-02-19 07:40:13 +0530

    Add auth routes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const router=express.Router();
+const {
+    signup,
+    login,
+} = require('../controllers/authController')
+const requireAuth =require('../middleware/requireAuth')
+
+//signup post request to create a new user in db
+router.post('/signup',signup)
+
+//login post request to authenticate a current user
+router.post('/login',login)
+
+//get request for user profile after authentication
+router.use('/profile',requireAuth)
+router.get('/profile',(req,res)=>{
+    res.status(200).json({msg:'authorised'})
+})
+
+
+
+
+module.exports=router
